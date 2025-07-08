f445e8dfa6394a49da812bc1828085ebe1d17bf5
Prasad Gahiwal <prasad.gahiwal@gmail.com>
2025-07-08 13:17:06 +0530

    Update ExampleCarouselImage.jsx

--- src/Components/ExampleCarouselImage.jsx
+++ src/Components/ExampleCarouselImage.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
 import '../Css/Home.css';
-export default function ExampleCarouselImage(props) {
+
+export default function ExampleCarouselImage({ im }) {
   return (
-    <div><img src={props.im}></img></div>
-  )
+    <div>
+      <img
+        src={im}
+        alt="carousel"
+        className="img-fluid"
+        style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+      />
+    </div>
+  );
 }
