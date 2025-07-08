ab14510c33cc9f865dbaf29dc0ef98a8fea547bd
Prasad Gahiwal <prasad.gahiwal@gmail.com>
2025-07-08 13:44:32 +0530

    Update Services.jsx

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,45 +1,47 @@
 import Carousel from 'react-bootstrap/Carousel';
-import img4 from "./img/4.jpg"
-import img5 from "./img/5.jpg"
-import img6 from "./img/6.jpg"
-
 
 function Services() {
   return (
     <>
-    <Carousel>
-      <Carousel.Item>
-      <img src={img4} alt="" style={{height:"800px",width:"3000px",}}/>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={img5} alt="" style={{height:"800px",width:"3000px"}}/>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            src="https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+            alt="First slide"
+            style={{ height: "800px", width: "100%", objectFit: "cover" }}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={img6} alt="" style={{height:"800px",width:"3000px"}}/>
+        <Carousel.Item>
+          <img
+            src="https://images.unsplash.com/photo-1612831455546-bb55b8c1ebc6"
+            alt="Second slide"
+            style={{ height: "800px", width: "100%", objectFit: "cover" }}
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Prsent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
-    
+        <Carousel.Item>
+          <img
+            src="https://images.unsplash.com/photo-1607746882042-944635dfe10e"
+            alt="Third slide"
+            style={{ height: "800px", width: "100%", objectFit: "cover" }}
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Prsent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </>
-    
   );
-  
 }
 
 export default Services;
